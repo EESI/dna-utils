45628e09b8f2e496a8766c442626dd6a13465286
Calvin Morrison <mutantturkey@gmail.com>
2013-10-02 18:03:13 -0400

    remove unused headers

--- kmer_utils.c
+++ kmer_utils.c
@@ -1,9 +1,3 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 // convert a string of k-mer size base-4 values  into a
 // base-10 index
 long num_to_index(const char *str, const int kmer, const long error_pos) {
@@ -27,9 +21,9 @@ long num_to_index(const char *str, const int kmer, const long error_pos) {
 // replaces values in a char array of ACGT's and others with
 // values that correspond to their base 4 value to be used in
 // num_to_index.
-void convert_kmer_to_num(char *str, const long length) {
+void convert_kmer_to_num(char *str, const unsigned long length) {
 
-  long i = 0;
+  unsigned long i = 0;
 
   for(i = 0; i < length; i++) {
     // this is _not_ portable, only works with ASCII values.
