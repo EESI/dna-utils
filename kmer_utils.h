510a8001307afc24199d22bbfb7730c972ca6d69
Calvin Morrison <mutantturkey@gmail.com>
2013-11-11 19:42:48 -0500

    fix warnings

--- kmer_utils.h
+++ kmer_utils.h
@@ -1,6 +1,7 @@
 // Kmer functions
 void convert_kmer_to_num(char *str, const unsigned long length);
 unsigned long num_to_index(const char *str, const int kmer, const long error_pos);
+char *index_to_kmer(unsigned long long index, long kmer);
 unsigned long long * get_kmer_counts_from_file(const char *fn, const int kmer);
 
 // Utility functions
