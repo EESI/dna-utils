6ea90212e404cf39debc14d82e02f6c284b05006
Calvin Morrison <mutantturkey@gmail.com>
2014-02-01 18:20:57 -0500

    change headers, organize

--- kmer_utils.h
+++ kmer_utils.h
@@ -1,8 +1,7 @@
 // Kmer functions
 void convert_kmer_to_num(char *str, const unsigned long length);
-unsigned long num_to_index(const char *str, const int kmer, const long error_pos, size_t *current_position);
+unsigned long num_to_index(const char *str, const int kmer, const long error_pos, long long *current_position);
 char *index_to_kmer(unsigned long long index, long kmer);
-unsigned long long * get_kmer_counts_from_file(const char *fn, const int kmer);
 
 // Utility functions
 char *strnstrip(const char *s, char *dest, int c, int len);
@@ -11,4 +10,6 @@ unsigned long long pow_four(unsigned long long x);
 // Variables
 const unsigned char alpha[256]; 
 
-unsigned long long load_specific_mers_from_file(char *fn, unsigned int kmer, size_t width, size_t *arr);
+// file loading functions
+unsigned long long load_specific_mers_from_file(const char *fn, unsigned int kmer, size_t width, size_t *arr);
+unsigned long long * get_kmer_counts_from_file(FILE *fh, const int kmer);
